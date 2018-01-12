d273e85ba057a9ab96f43b590f9670b43b711dd4
Sergio H. Nunez <sergio.nunez01@utrgv.edu>
2018-01-12 08:57:49 -0600

    Add files via upload
    This JSON file contains the same data as the other three files that has been named by their id numbers, those files get called on the first function, to populate the header's data.

---
+++ email_list.js
@@ -0,0 +1,20 @@
+[
+	{
+		"id": 100495,
+		"datetime": "12/28/2017 10:22 am",
+		"sender": "Checo Nunez",
+		"subject": "This is the first emil"
+	},
+	{
+		"id": 100496,
+		"datetime": "01/01/2018 11:33 pm",
+		"sender": "Clau J. Nunez",
+		"subject": "This is the second emil"
+	},
+	{
+		"id": 100497,
+		"datetime": "01/02/2018 3:33 am",
+		"sender": "Toby Nunez",
+		"subject": "This is the third emil"
+	}
+]
